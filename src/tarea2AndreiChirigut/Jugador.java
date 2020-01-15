5b8b0ed00d007cb7287cdd9277262953d6fe4e74
AndreiChirigut <andreichirigut07@gmail.com>
2020-01-15 09:57:32 +0100

    Primer comic, y atributos de la clase jugador

---
+++ src/tarea2AndreiChirigut/Jugador.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tarea2AndreiChirigut;
+
+/**
+ *
+ * @author Chiri
+ */
+public class Jugador {
+    String nombre;
+    int votos;
+    int id;
+    int edad;
+
+    public Jugador(String nombre, int votos, int id) {
+        this.nombre = nombre;
+        this.votos = votos;
+        this.id = id;
+    }
+}
