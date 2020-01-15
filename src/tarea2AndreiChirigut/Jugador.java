ff77ecb960c5999c9573dc8737d2089daceb0fc0
AndreiChirigut <andreichirigut07@gmail.com>
2020-01-15 10:26:31 +0100

    Eliminacion de getters y setters

--- src/tarea2AndreiChirigut/Jugador.java
+++ src/tarea2AndreiChirigut/Jugador.java
@@ -21,37 +21,7 @@ public class Jugador {
         this.id = id;
     }
 
-    public String getNombre() {
-        return nombre;
-    }
-
-    public void setNombre(String nombre) {
-        this.nombre = nombre;
-    }
-
-    public int getVotos() {
-        return votos;
-    }
-
-    public void setVotos(int votos) {
-        this.votos = votos;
-    }
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public int getEdad() {
-        return edad;
-    }
-
-    public void setEdad(int edad) {
-        this.edad = edad;
-    }
+    
     
     
 }
